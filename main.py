07b912f1997a7d128354b96f800fb26d867ffce8
aislaslo <islas.alex@gmail.com>
2025-01-06 18:27:37 -0600

    fixing jump spaces

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ def convert_currency (amount, from_currenty, to_currency):
     return result
 
 amount = float(input("Enter the amount to convert: "))
-
 from_currency = input("Enter the currency to convert from: ").upper()
-
 to_currency = input("Enter current to convert to: ").upper()
 
 converted_amount = convert_currency(amount, from_currency, to_currency)
